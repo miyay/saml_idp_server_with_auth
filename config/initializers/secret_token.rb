3b3ad065eed281d9efe609788d7ab70e4d60434a
miyay <yuuma_117@hotmail.com>
2014-01-02 23:59:36 +0900

    rails new saml_idp_server --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamlIdpServer::Application.config.secret_token = 'caaac77225a8365fb5282ae86473c6f10a2f13f5d62ad7e68a35660c42d466f99696c09857265d1201b62e51b0b82c4eb9e3d23041758d80e1a453f7afdec3da'
