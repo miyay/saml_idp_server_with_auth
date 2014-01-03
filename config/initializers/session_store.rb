f2a32fda7fae5c94cafdf09371980c1f03210073
miyay <yuuma_117@hotmail.com>
2014-01-03 17:33:41 +0900

    sessionをDBで管理

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-SamlIdpServer::Application.config.session_store :cookie_store, key: '_saml_idp_server_session'
+#SamlIdpServer::Application.config.session_store :cookie_store, key: '_saml_idp_server_session'
+SamlIdpServer::Application.config.session_store :active_record_store
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
