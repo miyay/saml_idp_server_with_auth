ab999eaaa649756e5bd904057ec41eed976cbdd5
miyay <yuuma_117@hotmail.com>
2014-01-21 23:54:26 +0900

    ユーザー認証を実装

--- app/controllers/saml_idp_controller.rb
+++ app/controllers/saml_idp_controller.rb
@@ -1,11 +1,12 @@
 class SamlIdpController < SamlIdp::IdpController
 
   def idp_authenticate(email, password)
-    true
+    user = ::User.where(email: params[:email]).first
+    user && user.valid_password?(params[:password]) ? user : nil
   end
 
   def idp_make_saml_response(user)
-    encode_SAMLResponse("you@example.com")
+    encode_SAMLResponse(user.email)
   end
 
 end
