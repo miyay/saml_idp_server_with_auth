2e1e72ea91a9ebf9082cf851d2d803da61e02663
miyay <yuuma_117@hotmail.com>
2014-02-01 16:03:18 +0900

    rootをrails_adminへ変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SamlIdpServer::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
+  root to: redirect("/admin")
+
   devise_for :users
 
   get '/saml/auth' => 'saml_idp#new'
